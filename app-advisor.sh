c93241a1cdfaefb0fcb0f67f2000cf42a961b327
Derek Beauregard <dbeauregard@gmail.com>
2025-03-06 12:24:35 -0700

    paramiterize input of SAA Version

--- app-advisor.sh
+++ app-advisor.sh
@@ -1,4 +1,4 @@
-VERSION=1.1.2
+echo "SAA VERSION=$VERSION"
 curl -L -H "Authorization: Bearer $ARTIFACTORY_TOKEN" -o /tmp/advisor-cli.tar -X GET https://packages.broadcom.com/artifactory/spring-enterprise/com/vmware/tanzu/spring/application-advisor-cli-linux/$VERSION/application-advisor-cli-linux-$VERSION.tar
 tar -xf /tmp/advisor-cli.tar --strip-components=1 -C /tmp
 install /tmp/advisor /usr/local/bin/advisor
